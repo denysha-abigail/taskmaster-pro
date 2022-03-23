9422008aa0f219cf0a422d7b5279befc6e27c0b1
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-23 02:45:08 -0400

    added drag and drop capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,7 +64,7 @@ $(".list-group").on("click", "p", function () {
 
 // triggers blur event as soon as the user interacts with anything other than the <textarea> element
 $(".list-group").on("blur", "textarea", function () {
-  
+
   // i.e.:
   // text = "Walk the dog";
   // status = "toDo";
@@ -101,7 +101,7 @@ $(".list-group").on("blur", "textarea", function () {
 });
 
 // due date was clicked
-$(".list-group").on("click", "span", function() {
+$(".list-group").on("click", "span", function () {
   // get current text
   var date = $(this)
     .text()
@@ -122,7 +122,7 @@ $(".list-group").on("click", "span", function() {
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -152,7 +152,83 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
-  
+// turns every element with the class list-group into a sortable list
+$(".card .list-group").sortable({
+  // links the sortable lists with any other lists that have the same class
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  // tells jQuery to create a copy of the dragged element and move the copy instead of the original; this is necessary to prevent click events from accidentally triggering on the original element.
+  helper: "clone",
+  // activate and deactivate events trigger once for all connected lists as soon as dragging starts and stops
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  // over and out events trigger when a dragged item enters or leaves a connected list
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  // update even triggers when the contents of a list have changed (i.e. the items were re-ordered, an item was removed, or an item was added)
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+    // the children() method returns an array of the list element's children (the <li> elements, labeled as li.list-group-item)
+    // loop over current set of children in sortable list
+    // jQuery's each() method will run a callback function for every item/element in the array; it's another form of looping, except that a function is now called on each loop iteration
+    $(this).children().each(function () {
+      // inside the callback function, $(this) actually refers to the child elements at that index; the nested $(this) refers to the task <li> element
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+      // trim down list's ID to match object property
+      var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+    });
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  // how to know which task the user wants to delete: you can't use this b/c it would simply refer to the #trash element that the event belongs to; the event.target wouldn't work either b/c #trash is also the target of the drop; drop(event, ui) --> ui variable is an object that contains a property called draggable; draggable, according to the jQuery documentation, is a jQuery object representing the draggable element
+  drop: function(event, ui) {
+    // removing a task from any of the lists triggers a sortable update(), meaning the sortable calls saveTasks() for us; jQuery's remove() method works just like a regular JavaScript remove()
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
   // clear values
