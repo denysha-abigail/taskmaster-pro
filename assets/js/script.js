b1f63cc656ddd5fc40f00e0f825b0b2ac90b04b5
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-24 02:07:08 -0400

    added moment.js capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var tasks = {};
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
@@ -13,9 +14,43 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
+};
+
+// when a task element is sent into the auditTask() function, we can get the date information and parse it into a Moment object using Moment.js
+var auditTask = function (taskEl) {
+  // get date from task element
+  // use jQuery to select the taskEl element and find the <span> element inside it, then retrieve the text value using .text(); we chained on a JavaScript (not jQuery) .trim() method to cut off any possible leading or trailing empty spaces
+  // when the .trim() method is used, it ensures no unnecessary empty spaces are in the beginning or end of the string
+  
+  var date = $(taskEl).find("span").text().trim();
+
+  // once the date information is stored into the date variable, we must pass that value into the moment() function to turn it into a Moment object
+  // first, we use the date variable we created from taskEl to make a new Moment object, configured for the user's local time using moment(date, "L")
+  // because the date variable does not specify a time of day (i.e. "11/23/2019"), the Moment object will default to 12:00am
+  // because work usually doesn't end end at 12:00am, we convert it to 5:00pm of that day instead, using the .set("hour", 17) method (value 17 in 24-hr time, or 5:00pm)
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  // remove any old classes from element if they were already in place (i.e. if we update the due date from yesterday to a week from now, that red background will be removed, as it will no longer be overdue)
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  // moment()isAfter(time) code inside the if statement is known as the query method; this means that we can perform simple true or false checks on the date for more information about it
+  // when read from left to right, tthe isAfter() gets the current time from moment() and checks if that value comes later than the value of the time variable (we're checking if the current date and time are later than the date and time we pulled from taskEl; if so, the date and time from taskEl are in the past, and we add the list-group-item-danger bootstrap class to the entire task element --> thus, giving it a red background, to let users know the date has passed)
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+    // to see how many days away the due date is
+    // performs from left to right --> when we use moment() to get right now and use .diff() afterwards to get the difference of right now to a day in the future, we'll get a negative number back (we have to check if the difference is >= -2)
+    // testing for a number less than +2 not a number greater than -2
+    // the abs() method ensures we're getting the absolute value of that number
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 var loadTasks = function () {
@@ -117,12 +152,23 @@ $(".list-group").on("click", "span", function () {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    // onClose allows us to execute a function when the date picker closes; it may close when a user clicks anywhere on the page outside the date picker; now when we go to update a due date and decide to leave it as is, we can click off of the date picker and the <span> element will reappear with the original date; by adding the onClose method, we can instruct the dateInput element to trigger its own change event and execute the callback function tied to it
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+// listen for a change rather than a blur; useful for when we click off of the date picker because we've decided not to edit the due date --> the <input> element remains and doesn't switch back to the actua; date's <span> element
+$(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -150,6 +196,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // pass task's <li> element into auditTask() to check new date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // turns every element with the class list-group into a sortable list
@@ -216,15 +265,15 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   // how to know which task the user wants to delete: you can't use this b/c it would simply refer to the #trash element that the event belongs to; the event.target wouldn't work either b/c #trash is also the target of the drop; drop(event, ui) --> ui variable is an object that contains a property called draggable; draggable, according to the jQuery documentation, is a jQuery object representing the draggable element
-  drop: function(event, ui) {
+  drop: function (event, ui) {
     // removing a task from any of the lists triggers a sortable update(), meaning the sortable calls saveTasks() for us; jQuery's remove() method works just like a regular JavaScript remove()
     ui.draggable.remove();
     console.log("drop");
   },
-  over: function(event, ui) {
+  over: function (event, ui) {
     console.log("over");
   },
-  out: function(event, ui) {
+  out: function (event, ui) {
     console.log("out");
   }
 })
@@ -233,6 +282,12 @@ $("#trash").droppable({
 $("#task-form-modal").on("show.bs.modal", function () {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
+});
+
+// add date picker
+$("#modalDueDate").datepicker({
+  // set key-value pair; minDate option with a value of 1 --> indicates how many days after the current date we want the limit to kick in; minDate of 1 allows you to pick a date starting tomorrow
+  minDate: 1
 });
 
 // modal is fully visible
